aa7c5796d7723a8a4f8fa661af7a46b1662cb125
Andreas Stocker <stoand@protonmail.com>
2023-07-03 11:57:07 +0200

    setup

---
+++ src/basic.spec.ts
@@ -0,0 +1,9 @@
+import {describe, expect, test} from '@jest/globals';
+
+describe('asdf', () => {
+    test('works', () => {
+        // expect(1)
+        console.log('asdf');
+        expect(1).toBe(2);
+    });
+});
